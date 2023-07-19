92067051531edbd3b5b7a72c7e4cb6b015bb735c
Yan Heng <yanheng@siactpower.com>
2023-07-19 14:51:00 +0800

    feat: 解决构造后无法编辑问题

--- packages/core/src/context.ts
+++ packages/core/src/context.ts
@@ -4,38 +4,36 @@ import { fabric } from 'fabric';
 import { Event } from './types';
 
 export class Context extends BaseService<Event.ContextArgs> {
-  private canvasElement: HTMLCanvasElement;
-  private canvas: fabric.Canvas;
+  public canvas?: fabric.Canvas;
+  private canvasEl: HTMLCanvasElement;
 
   constructor() {
     super();
-    this.canvasElement = document.createElement('canvas');
-
-    // 创建Fabric Canvas实例
-    this.canvas = new fabric.Canvas(this.canvasElement);
-    this.canvasElement.style.backgroundColor = 'lightgray';
-    this.canvasElement.style.position = 'static';
-    this.canvasElement.style.left = 'auto';
-    this.canvasElement.style.top = 'auto';
-    const canvas = this.canvas;
-    const rect = new fabric.Rect({
-      left: 100,
-      top: 100,
-      width: 200,
-      height: 100,
-      fill: 'red',
-    });
-
-    canvas.add(rect);
-    canvas.renderAll();
+    this.canvasEl = document.createElement('canvas');
   }
 
   public mount(element: HTMLElement) {
-    element.appendChild(this.canvasElement);
-    this.canvas.setDimensions({
-      width: element.clientWidth,
-      height: element.clientHeight,
+    element.appendChild(this.canvasEl);
+    this.canvas = new fabric.Canvas(this.canvasEl, {
+      backgroundColor: 'lightgray',
+      width: 500,
+      height: 500,
     });
+    this.addRect();
+  }
+
+  public addRect() {
+    const rect = new fabric.Rect({
+      left: 100,
+      top: 100,
+      fill: 'red',
+      width: 20,
+      height: 20,
+      hasControls: true,
+      hasBorders: true,
+    });
+
+    this.canvas?.add(rect);
   }
 }
 
