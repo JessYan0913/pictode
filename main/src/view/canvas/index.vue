ae4b6ea281fd21f10472cba9a6cc830e05beacc6
JessYan <52144974+JessYan0913@users.noreply.github.com>
2023-07-20 21:13:38 +0800

    update: 更新组件

--- main/src/view/canvas/index.vue
+++ main/src/view/canvas/index.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 import { App } from '@pictode/core';
-import { History } from '@pictode/plugin-history';
 
 const containerRef = ref<HTMLDivElement>();
 
-const history = new History();
-
 const app = new App();
-app.use(history);
-app.undo();
 onMounted(() => {
   if (containerRef.value) {
     app.mount(containerRef.value);
