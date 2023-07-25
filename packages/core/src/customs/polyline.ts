f5e7c476a8c4b9b78a02466023b2671b4eaba57f
Yan Heng <yanheng@siactpower.com>
2023-07-25 11:47:50 +0800

    update: 重构polyline代码

--- packages/core/src/customs/polyline.ts
+++ packages/core/src/customs/polyline.ts
@@ -1,6 +1,14 @@
 import { fabric } from 'fabric';
 
 export class Polyline extends fabric.Polyline {
+  public onSelect(options: { e?: Event | undefined }): boolean {
+    return super.onSelect(options);
+  }
+
+  public onDeselect(options: { e?: Event | undefined; object?: fabric.Object | undefined }): boolean {
+    return super.onDeselect(options);
+  }
+
   public render(ctx: CanvasRenderingContext2D): void {
     super.render(ctx);
   }
