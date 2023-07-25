857e61bea905d39d41961e05510f1d6a8516f61e
JessYan <52144974+JessYan0913@users.noreply.github.com>
2023-07-25 20:05:55 +0800

    feat: 解决控制点在变换后不正确问题

--- packages/core/src/customs/polyline.ts
+++ packages/core/src/customs/polyline.ts
@@ -7,7 +7,7 @@ export class Polyline extends fabric.Polyline {
       (controls, point, index) => {
         controls[`p${index}`] = new fabric.Control({
           actionName: 'modifyPolyline',
-          positionHandler(dim, finalMatrix, fabricObject: Polyline) {
+          positionHandler: (dim, finalMatrix, fabricObject: Polyline): fabric.Point => {
             const x = point.x - fabricObject.pathOffset.x;
             const y = point.y - fabricObject.pathOffset.y;
             return fabric.util.transformPoint(
@@ -18,10 +18,13 @@ export class Polyline extends fabric.Polyline {
               )
             );
           },
-          actionHandler: (eventData, transformData, x, y) => {
+          actionHandler: (eventData, transformData, x, y): boolean => {
+            const polyline = transformData.target as Polyline;
+            const invertedMatrix = fabric.util.invertTransform(polyline.calcTransformMatrix());
+            const transformedPoint = fabric.util.transformPoint(new fabric.Point(x, y), invertedMatrix);
             // 更新点的位置
-            points[index].x = x;
-            points[index].y = y;
+            points[index].x = transformedPoint.x + polyline.pathOffset.x;
+            points[index].y = transformedPoint.y + polyline.pathOffset.y;
             // 重新设置points
             this.points = points;
             // 更新坐标
