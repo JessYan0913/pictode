d4edca7c67e3440269c76a384e3702194eaeb5ae
Yan Heng <yanheng@siactpower.com>
2023-07-25 14:22:20 +0800

    feat: 增加顶点控制器

--- packages/core/src/customs/polyline.ts
+++ packages/core/src/customs/polyline.ts
@@ -2,6 +2,27 @@ import { fabric } from 'fabric';
 
 export class Polyline extends fabric.Polyline {
   public onSelect(options: { e?: Event | undefined }): boolean {
+    const points = this.points ?? [];
+    this.controls = points.reduce<Record<string, fabric.Control>>(
+      (controls, point, index) => {
+        controls[`p${index}`] = new fabric.Control({
+          actionName: 'modifyPolyline',
+          positionHandler(dim, finalMatrix, fabricObject: Polyline) {
+            const x = point.x - fabricObject.pathOffset.x;
+            const y = point.y - fabricObject.pathOffset.y;
+            return fabric.util.transformPoint(
+              new fabric.Point(x, y),
+              fabric.util.multiplyTransformMatrices(
+                fabricObject.canvas?.viewportTransform ?? [],
+                fabricObject.calcTransformMatrix()
+              )
+            );
+          },
+        });
+        return controls;
+      },
+      { ...this.controls }
+    );
     return super.onSelect(options);
   }
 
