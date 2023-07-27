0576e6ec9116fa3552db4485c52d062e89cb1dec
JessYan <52144974+JessYan0913@users.noreply.github.com>
2023-07-27 22:20:51 +0800

    feat: app新增插件方法

--- packages/plugin-history/src/methods.ts
+++ packages/plugin-history/src/methods.ts
@@ -20,3 +20,35 @@ App.prototype.undo = function (step: number = 1): App {
   }
   return this;
 };
+
+App.prototype.redo = function (step: number = 1): App {
+  const history = this.getPlugin('history') as History;
+  if (history) {
+    history.redo(step);
+  }
+  return this;
+};
+
+App.prototype.canUndo = function (): boolean {
+  const history = this.getPlugin('history') as History;
+  if (history) {
+    return history.canUndo();
+  }
+  return false;
+};
+
+App.prototype.canRedo = function (): boolean {
+  const history = this.getPlugin('history') as History;
+  if (history) {
+    return history.canRedo();
+  }
+  return false;
+};
+
+App.prototype.jump = function (id: number): App {
+  const history = this.getPlugin('history') as History;
+  if (history) {
+    history.jump(id);
+  }
+  return this;
+};
