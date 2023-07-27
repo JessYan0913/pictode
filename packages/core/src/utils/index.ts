7618ad3f7d37077ebc20a8f8eeb1c66bff7ba268
Yan Heng <yanheng@siactpower.com>
2023-07-27 17:45:02 +0800

    update: 更新默认值信息

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -11,7 +11,7 @@ export const DEFAULT_APP_OPTION: AppOption & { controls: ControlsOption } = {
     cornerColor: 'rgb(157, 157, 231)',
     cornerStrokeColor: 'rgb(157, 157, 231)',
     cornerStyle: 'circle',
-    cornerSize: 6,
+    cornerSize: 8,
     rotatingPointOffset: 10,
     transparentCorners: true,
   },
