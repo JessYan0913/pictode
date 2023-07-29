0d2eb8d4b494144f64895a044c22af8a783a2f9a
Yan Heng <yanheng@siactpower.com>
2023-07-29 14:32:06 +0800

    update: 更新select-tool

--- packages/core/src/tools/select-tool.ts
+++ packages/core/src/tools/select-tool.ts
@@ -1,20 +1,10 @@
-import Konva from 'konva';
-
 import { Tool } from '../types';
 
 export const selectTool: Tool = {
   name: 'selectTool',
-  onActive(app) {
-    app.select();
-  },
+  onActive() {},
   onInactive() {},
-  onMouseDown({ event, app }) {
-    if (event.target instanceof Konva.Stage) {
-      app.select();
-    } else {
-      app.select(event.target);
-    }
-  },
+  onMouseDown() {},
 };
 
 export default selectTool;
