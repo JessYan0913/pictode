0a97814cff7cac8a5c50557c208f5966f3344316
Yan Heng <yanheng@siactpower.com>
2023-07-29 10:50:52 +0800

    feat: 实现ellipse-tool

--- packages/core/src/tools/ellipse-tool.ts
+++ packages/core/src/tools/ellipse-tool.ts
@@ -1,26 +1,68 @@
 import Konva from 'konva';
 
+import App from '../app';
 import { Ellipse } from '../customs/ellipse';
-import { Tool } from '../types';
+import { AppMouseEvent, Tool } from '../types';
+
+import { selectTool } from './select-tool';
 
 class EllipseTool implements Tool {
   public name: string = 'ellipseTool';
   private startPointer: Konva.Vector2d = { x: 0, y: 0 };
   private ellipse: Ellipse | null = null;
 
-  public onActive(): void {
-    throw new Error('Method not implemented.');
+  public onActive(app: App): void {
+    app.select();
   }
 
   public onInactive(): void {
-    throw new Error('Method not implemented.');
+    this.ellipse = null;
+    this.startPointer = { x: 0, y: 0 };
   }
 
-  public onMouseDown(): void {}
+  public onMouseDown({ app }: AppMouseEvent): void {
+    this.startPointer = app.pointer;
+    this.ellipse = new Ellipse({
+      x: this.startPointer.x,
+      y: this.startPointer.y,
+      radiusX: 0,
+      radiusY: 0,
+      fill: 'transparent',
+      stroke: 'black',
+      strokeWidth: 2,
+    });
+    app.add(this.ellipse);
+  }
 
-  public onMouseMove(): void {}
+  public onMouseMove({ app }: AppMouseEvent): void {
+    if (!this.ellipse) {
+      return;
+    }
+    // 计算起点和当前鼠标位置之间的距离
+    const dx = app.pointer.x - this.startPointer.x;
+    const dy = app.pointer.y - this.startPointer.y;
 
-  public onMouseUp(): void {}
+    // 计算椭圆的宽度和高度的绝对值
+    const radiusX = Math.abs(dx) / 2;
+    const radiusY = Math.abs(dy) / 2;
+
+    // 根据起点和鼠标位置计算椭圆的中心位置
+    const centerX = this.startPointer.x + dx / 2;
+    const centerY = this.startPointer.y + dy / 2;
+    this.ellipse.x(centerX);
+    this.ellipse.y(centerY);
+    this.ellipse.radiusX(radiusX);
+    this.ellipse.radiusY(radiusY);
+    app.render();
+  }
+
+  public onMouseUp({ app }: AppMouseEvent): void {
+    if (!this.ellipse) {
+      return;
+    }
+    app.select(this.ellipse);
+    app.setTool(selectTool);
+  }
 }
 
 export const ellipseTool = new EllipseTool();
