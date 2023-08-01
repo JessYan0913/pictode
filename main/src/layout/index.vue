582842699b2c5187b8bf99688296896ae99d66c8
JessYan <52144974+JessYan0913@users.noreply.github.com>
2023-08-01 22:19:15 +0800

    update: 更新layout布局

--- main/src/layout/index.vue
+++ main/src/layout/index.vue
@@ -1,26 +1,16 @@
 <script lang="ts" setup>
 import { useTheme } from '@/hooks/useTheme';
 
-import Header from './Header.vue';
 import Main from './Main.vue';
-import Menu from './Menu.vue';
 
 useTheme();
 </script>
 
 <template>
   <el-container class="layout">
-    <el-aside width="200px">
-      <Menu />
-    </el-aside>
-    <el-container>
-      <el-header>
-        <Header />
-      </el-header>
-      <el-main>
-        <Main />
-      </el-main>
-    </el-container>
+    <el-main>
+      <Main />
+    </el-main>
   </el-container>
 </template>
 
