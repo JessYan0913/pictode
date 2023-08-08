3d8d70e48a63d2ea7a0ee46e5c37db93d642c526
Yan Heng <yanheng@siactpower.com>
2023-08-08 10:44:39 +0800

    update: 删除无用样式

--- main/src/components/Tools.vue
+++ main/src/components/Tools.vue
@@ -16,43 +16,4 @@ const { currentTool, tools } = usePictode();
   </RadioGroup>
 </template>
 
-<style scoped lang="scss">
-.tools-horizontal {
-  width: 100%;
-  display: grid;
-  grid-template-rows: auto;
-  grid-auto-flow: column;
-  gap: calc(0.25rem * 1);
-  justify-items: center;
-
-  & > .tool-icon {
-    & > input {
-      position: absolute;
-      opacity: 0;
-    }
-
-    & > input:checked + div {
-      background: #e3e2fe;
-    }
-  }
-}
-
-.tool-icon {
-  border-radius: 0.5rem;
-  display: inline-flex;
-  align-items: center;
-  position: relative;
-  cursor: pointer;
-  user-select: none;
-  -webkit-tap-highlight-color: transparent;
-
-  div {
-    width: 2.25rem;
-    height: 2.25rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 0.5rem;
-  }
-}
-</style>
+<style scoped lang="scss"></style>
