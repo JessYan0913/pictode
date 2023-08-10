8feee3e2f47ada22526249b8fcea2ef38bca243a
JessYan <52144974+JessYan0913@users.noreply.github.com>
2023-08-10 22:02:24 +0800

    update: 删除无用类型声明

--- main/src/components/Property.vue
+++ main/src/components/Property.vue
@@ -1,22 +1,7 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-import { RadioGroup, RadioGroupOption } from '@headlessui/vue';
-
-const plan = ref('startup');
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline bg-blue-500 hover:bg-sky-700">Hello world!</h1>
-  <div class="padding-[20px]">
-    <RadioGroup v-model="plan">
-      <div class="pictode-radio-group">
-        <RadioGroupOption v-slot="{ checked }" value="startup">
-          <span :class="['pictode-radio-group-option', checked ? 'checked' : '']">Startup</span>
-        </RadioGroupOption>
-        <RadioGroupOption v-slot="{ checked }" value="business">
-          <span :class="['pictode-radio-group-option', checked ? 'checked' : '']">Business</span>
-        </RadioGroupOption>
-      </div>
-    </RadioGroup>
+  <div class="p-1">
+    <h1 class="text-3xl font-bold underline bg-blue-500 hover:bg-sky-700">Hello world!</h1>
   </div>
 </template>
--- main/src/constants/injection-keys.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionKey } from 'vue';
-
-import { RadioCtx } from '@/types';
-
-export const RadioCtxKey: InjectionKey<RadioCtx<unknown>> = Symbol('RadioCtx');
