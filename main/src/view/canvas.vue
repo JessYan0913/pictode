55e9bc8552d178b4ed14ccdd23bbccb8353dd413
Yan Heng <yanheng@siactpower.com>
2023-08-11 17:02:33 +0800

    update: 更新布局

--- main/src/view/canvas.vue
+++ main/src/view/canvas.vue
@@ -40,30 +40,30 @@ const formConfig: FormConfig = [
 
 <template>
   <div class="w-full h-full">
-    <div class="absolute left-0 top-0 right-0 bottom-0 p-10 z-10 pointer-events-none">
-      <div class="grid grid-cols-3 gap-12 items-start cursor-default pointer-events-none font-AlimamaFangYuanTi">
-        <section class="justify-self-start pointer-events-auto">
-          <Menu></Menu>
-        </section>
-        <section
-          class="justify-center bg-opacity-88 shadow-md rounded-lg p-2 ring-1 ring-black ring-opacity-5 transition-shadow"
-        >
-          <Tools class="pointer-events-auto w-full"></Tools>
-        </section>
-        <section
-          class="justify-self-end bg-opacity-88 shadow-md rounded-lg p-2 ring-1 ring-black ring-opacity-5 transition-shadow"
-        >
-          <div class="grid grid-flow-col gap-4 w-full pointer-events-auto">
-            <Button class="w-8 h-8" icon="undo" @click="app.undo()"></Button>
-            <Button class="w-8 h-8" icon="redo" @click="app.redo()"></Button>
-          </div>
-        </section>
-      </div>
-      <div class="grid grid-cols-3 gap-12 items-start cursor-default">
-        <div class="pointer-events-auto">
-          <Form :config="formConfig" label-position="top"></Form>
+    <div
+      class="absolute left-0 top-0 right-0 bottom-0 p-8 z-10 pointer-events-none grid grid-cols-3 grid-rows-[auto_1fr_50px] gap-12"
+    >
+      <section class="row-start-1 col-start-1 justify-self-start pointer-events-auto">
+        <Menu></Menu>
+      </section>
+      <section
+        class="bg-opacity-88 row-start-1 col-start-2 justify-self-stretch place-content-center shadow-md rounded-lg p-2 ring-1 ring-black ring-opacity-5 transition-shadow"
+      >
+        <Tools class="pointer-events-auto w-full"></Tools>
+      </section>
+      <section
+        class="bg-opacity-88 row-start-1 col-start-3 justify-self-end shadow-md rounded-lg p-2 ring-1 ring-black ring-opacity-5 transition-shadow"
+      >
+        <div class="grid grid-flow-col gap-4 w-full pointer-events-auto">
+          <Button class="w-8 h-8" icon="undo" @click="app.undo()"></Button>
+          <Button class="w-8 h-8" icon="redo" @click="app.redo()"></Button>
         </div>
-      </div>
+      </section>
+      <section
+        class="bg-opacity-88 row-start-2 col-start-3 shadow-md rounded-lg p-2 ring-1 ring-black ring-opacity-5 transition-shadow pointer-events-auto"
+      >
+        <Form :config="formConfig" label-position="top"></Form>
+      </section>
     </div>
     <div ref="canvasRef" class="w-full h-full"></div>
   </div>
