b37c8c8c05d7ec1e34c2ffe2c8e1d8da27e7c864
Yan Heng <yanheng@siactpower.com>
2023-08-14 10:26:05 +0800

    update: 删除无用代码

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,6 @@
 export { default as Konva } from 'konva';
 export * from './app';
 export * from './types';
-export * from './decorators/expand-app';
 export * from './tools/rect-tool';
 export * from './tools/select-tool';
 export * from './tools/ellipse-tool';
