d5817c1523447eee6e08cd6021d2c5456e914f00
Yan Heng <yanheng@siactpower.com>
2023-08-14 10:05:06 +0800

    update: 删除无用代码

--- packages/core/src/decorators/expand-app.ts
+++
@@ -1,9 +0,0 @@
-import { App } from '../app';
-import { Plugin } from '../types';
-
-export function expandApp<T extends Plugin = Plugin>(methodName?: string) {
-  return function (target: T, propertyKey: string, descriptor: PropertyDescriptor) {
-    methodName = methodName ?? propertyKey;
-    Reflect.set(App.prototype, methodName, descriptor.value);
-  };
-}
