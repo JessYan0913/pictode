80809beddcc0166e3f6aab64927c11f6fd4b12f4
JessYan <52144974+JessYan0913@users.noreply.github.com>
2023-08-20 22:16:22 +0800

    update: 更新导出格式选择

--- main/src/components/ExportImage.vue
+++ main/src/components/ExportImage.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/vue';
+import { CheckIcon, ChevronUpDownIcon } from '@heroicons/vue/24/solid';
 
 import Dialog from '@/components/Dailog.vue';
 import RadioGroup from '@/components/RadioGroup.vue';
@@ -30,6 +32,9 @@ const dialogVisible = computed<boolean>({
 const enabled = ref<boolean>(false);
 const pixelRatio = ref<number>(2);
 
+const people = [{ name: 'PNG' }, { name: 'SVG' }, { name: 'JPG' }];
+const selectedPerson = ref(people[0]);
+
 function closeModal() {
   dialogVisible.value = false;
 }
@@ -58,6 +63,51 @@ function closeModal() {
           <RadioGroupOption :value="2" class="font-mono text-xs">{{ '2x' }}</RadioGroupOption>
           <RadioGroupOption :value="3" class="font-mono text-xs">{{ '3x' }}</RadioGroupOption>
         </RadioGroup>
+      </div>
+      <div class="flex flex-row justify-between items-center">
+        <label>图片格式</label>
+        <Listbox v-model="selectedPerson" class="w-24">
+          <div class="relative mt-1">
+            <ListboxButton
+              class="relative w-full cursor-default rounded ring-1 ring-black ring-opacity-5 p-0.5 py-2 pl-3 pr-10 text-left focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+            >
+              <span class="block truncate">{{ selectedPerson.name }}</span>
+              <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                <ChevronUpDownIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+              </span>
+            </ListboxButton>
+
+            <transition
+              leave-active-class="transition duration-100 ease-in"
+              leave-from-class="opacity-100"
+              leave-to-class="opacity-0"
+            >
+              <ListboxOptions
+                class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+              >
+                <ListboxOption
+                  v-slot="{ active, selected }"
+                  v-for="person in people"
+                  :key="person.name"
+                  :value="person"
+                  as="template"
+                >
+                  <li
+                    :class="[
+                      active ? 'bg-amber-100 text-amber-900' : 'text-gray-900',
+                      'relative cursor-default select-none py-2 pl-10 pr-4',
+                    ]"
+                  >
+                    <span :class="[selected ? 'font-medium' : 'font-normal', 'block truncate']">{{ person.name }}</span>
+                    <span v-if="selected" class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                      <CheckIcon class="h-5 w-5" aria-hidden="true" />
+                    </span>
+                  </li>
+                </ListboxOption>
+              </ListboxOptions>
+            </transition>
+          </div>
+        </Listbox>
       </div>
       <div class="grow flex flex-col justify-end">
         <button
