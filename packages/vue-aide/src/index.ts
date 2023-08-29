7cbecf77d36b25f4b05ead9935fef2d6cf911346
Yan Heng <yanheng@siactpower.com>
2023-08-29 11:43:30 +0800

    feat: 增加injectWithSelf钩子

--- packages/vue-aide/src/index.ts
+++ packages/vue-aide/src/index.ts
@@ -1,13 +1,18 @@
-import { inject, InjectionKey, isRef, MaybeRefOrGetter, Ref, unref } from 'vue';
+import { getCurrentInstance, inject, InjectionKey, isRef, MaybeRefOrGetter, Ref, unref } from 'vue';
 
 export * from './hooks';
 
-export const injectStrict = <T>(key: InjectionKey<T>, fallback?: T) => {
+export const injectStrict = <T>(key: InjectionKey<T>, fallback?: T): T => {
   const resolved = inject(key, fallback);
   if (!resolved) {
     throw new Error(`Could not resolve ${key.description}`);
   }
   return resolved;
+};
+
+export const injectWithSelf = <T>(key: InjectionKey<T>, fallback?: T): T | undefined => {
+  const vm = getCurrentInstance() as any;
+  return vm?.provides[key as any] || inject(key, fallback);
 };
 
 export const unravel = <T>(value: MaybeRefOrGetter<T>): T => {
