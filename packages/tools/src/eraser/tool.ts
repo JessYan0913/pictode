a987d9fdae8b09c4cd9c52ca3755ed051826ca60
Yan Heng <yanheng@siactpower.com>
2023-09-08 11:30:34 +0800

    update: 橡皮擦支持滑动删除多个

--- packages/tools/src/eraser/tool.ts
+++ packages/tools/src/eraser/tool.ts
@@ -3,33 +3,66 @@ import { Konva, KonvaNode, Tool, ToolEvent, ToolHooks } from '@pictode/core';
 export class EraserTool implements Tool {
   public name: string = 'eraserTool';
   public hooks?: ToolHooks | undefined;
-  private target: KonvaNode | null = null;
-  private targetOpacity: number = 1;
+  private targetMap: Map<
+    string,
+    {
+      node: KonvaNode;
+      opacity: number;
+    }
+  > = new Map();
+  private isEraser: boolean = false;
 
   constructor(options: { hooks?: ToolHooks }) {
     this.hooks = options.hooks;
   }
 
   public mousedown({ event }: ToolEvent) {
+    this.isEraser = true;
     if (event.target instanceof Konva.Stage) {
       return;
     }
-    this.target = event.target;
-    this.targetOpacity = this.target.opacity();
-    event.target.opacity(this.targetOpacity / 2);
+    this.targetMap.set(event.target.id(), {
+      node: event.target,
+      opacity: event.target.attrs.opacity,
+    });
+    event.target.opacity(0.3);
   }
 
   public mouseup({ app }: ToolEvent) {
-    if (!this.target) {
+    if (!this.targetMap.size) {
       return;
     }
-    this.target.opacity(this.targetOpacity);
-    app.remove(this.target);
-    this.target = null;
+    app.remove(
+      ...[...this.targetMap.values()].map(({ node, opacity }) => {
+        node.opacity(opacity);
+        return node;
+      })
+    );
+    this.targetMap.clear();
+    this.isEraser = false;
+  }
+
+  public mousemove({ event }: ToolEvent) {
+    if (!this.isEraser) {
+      return;
+    }
+    if (event.target instanceof Konva.Stage) {
+      return;
+    }
+    const target = this.targetMap.get(event.target.id());
+    if (target) {
+      return;
+    }
+    this.targetMap.set(event.target.id(), {
+      node: event.target,
+      opacity: event.target.attrs.opacity,
+    });
+    event.target.opacity(0.3);
   }
 
   public enableChanged(): void {
-    this.target = null;
+    this.targetMap.clear();
+    this.isEraser = false;
   }
 }
 
