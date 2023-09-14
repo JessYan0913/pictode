ea017ea83df654ffad835baaaf53adc8032b710a
Yan Heng <yanheng@siactpower.com>
2023-09-14 10:41:13 +0800

    update: 更新右侧属性面板布局逻辑

--- pictode/src/view/canvas/components/PropertyPanel.vue
+++ pictode/src/view/canvas/components/PropertyPanel.vue
@@ -11,68 +11,9 @@ const app = injectStrict(PictodeAppKey);
 const selected = injectStrict(PictodeSelectedKey);
 const { panelFormConfig, panelFormModel } = injectStrict(PictodePanelFormKey);
 
-const alignButtons = [
-  {
-    icon: 'align-left',
-    title: '左对齐',
-    action: () => {
-      app.alignLeft(selected.value);
-    },
-  },
-  {
-    icon: 'align-horizontally',
-    title: '水平居中',
-    action: () => {
-      app.alignCenterX(selected.value);
-    },
-  },
-  {
-    icon: 'align-right',
-    title: '右对齐',
-    action: () => {
-      app.alignRight(selected.value);
-    },
-  },
-  {
-    icon: 'distribute-horizontal-spacing',
-    title: '水平分布',
-    action: () => {
-      app.dispersionX(selected.value);
-    },
-  },
-  {
-    icon: 'align-top',
-    title: '顶对齐',
-    action: () => {
-      app.alignTop(selected.value);
-    },
-  },
-  {
-    icon: 'align-vertically',
-    title: '垂直居中',
-    action: () => {
-      app.alignCenterY(selected.value);
-    },
-  },
-  {
-    icon: 'align-bottom',
-    title: '底对齐',
-    action: () => {
-      app.alignBottom(selected.value);
-    },
-  },
-  {
-    icon: 'distribute-vertical-spacing',
-    title: '垂直分布',
-    action: () => {
-      app.dispersionY(selected.value);
-    },
-  },
-];
-
 const { moveBottom, moveDown, moveUp, moveTop } = useHotKeyList(app, selected);
 
-const layerButtons = [
+const layerButtons = computed(() => [
   {
     icon: 'bring-to-front-one',
     title: `${moveBottom.directions}-${moveBottom.hotKey?.join('+')}`,
@@ -93,9 +34,76 @@ const layerButtons = [
     title: `${moveTop.directions}-${moveTop.hotKey?.join('+')}`,
     action: moveTop,
   },
-];
+]);
 
-const showAlignButtons = computed<boolean>(() => selected.value.length > 2);
+const alignButtons = computed(() => [
+  {
+    icon: 'align-left',
+    title: '左对齐',
+    visible: selected.value.length > 1,
+    action: () => {
+      app.alignLeft(selected.value);
+    },
+  },
+  {
+    icon: 'align-horizontally',
+    title: '水平居中',
+    visible: selected.value.length > 1,
+    action: () => {
+      app.alignCenterX(selected.value);
+    },
+  },
+  {
+    icon: 'align-right',
+    title: '右对齐',
+    visible: selected.value.length > 1,
+    action: () => {
+      app.alignRight(selected.value);
+    },
+  },
+  {
+    icon: 'distribute-horizontal-spacing',
+    title: '水平分布',
+    visible: selected.value.length > 2,
+    action: () => {
+      app.dispersionX(selected.value);
+    },
+  },
+  {
+    icon: 'align-top',
+    title: '顶对齐',
+    visible: selected.value.length > 1,
+    action: () => {
+      app.alignTop(selected.value);
+    },
+  },
+  {
+    icon: 'align-vertically',
+    title: '垂直居中',
+    visible: selected.value.length > 1,
+    action: () => {
+      app.alignCenterY(selected.value);
+    },
+  },
+  {
+    icon: 'align-bottom',
+    title: '底对齐',
+    visible: selected.value.length > 1,
+    action: () => {
+      app.alignBottom(selected.value);
+    },
+  },
+  {
+    icon: 'distribute-vertical-spacing',
+    title: '垂直分布',
+    visible: selected.value.length > 2,
+    action: () => {
+      app.dispersionY(selected.value);
+    },
+  },
+]);
+
+const showAlignButtons = computed<boolean>(() => selected.value.length > 1);
 
 const handleFormChange = (value: any) => {
   app.update(
@@ -117,15 +125,19 @@ const handleFormChange = (value: any) => {
     <div v-if="showAlignButtons" class="flex flex-col content-start gap-1 px-1 py-2 select-none">
       <label class="text-start text-sm text-gray-600">对齐</label>
       <div class="flex flex-row justify-start flex-wrap gap-2">
-        <button
-          v-for="(button, index) in alignButtons"
-          :key="index"
-          class="border rounded-lg inline-flex items-center relative cursor-pointer select-none p-2 hover:bg-slate-200"
-          :title="button.title"
-          @click="button.action"
-        >
-          <SvgIcon :name="button.icon" class="cursor-pointer"></SvgIcon>
-        </button>
+        <div v-for="(button, index) in alignButtons" :key="index">
+          <button
+            v-if="button.visible"
+            class="border rounded-lg inline-flex items-center relative cursor-pointer select-none p-2 hover:bg-slate-200"
+            :title="button.title"
+            @click="button.action"
+          >
+            <SvgIcon :name="button.icon" class="cursor-pointer"></SvgIcon>
+          </button>
+          <div v-else class="select-none p-2">
+            <SvgIcon name=""></SvgIcon>
+          </div>
+        </div>
       </div>
     </div>
     <div class="flex flex-col content-start gap-1 px-1 py-2 select-none">
