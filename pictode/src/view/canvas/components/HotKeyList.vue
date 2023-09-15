35a4ad1aae4d434a24a1d30181f869e58fdd1889
Yan Heng <yanheng@siactpower.com>
2023-09-15 11:28:24 +0800

    update: 滚动条开启圆角配置

--- pictode/src/view/canvas/components/HotKeyList.vue
+++ pictode/src/view/canvas/components/HotKeyList.vue
@@ -43,10 +43,10 @@ const closeModal = () => {
   <Dialog :visible="dialogVisible" @close="closeModal">
     <template #title> 帮助中心 </template>
     <div
-      class="w-full min-w-fit md:min-w-[60rem] h-[50rem] p-4 overflow-auto scrollbar scrollbar-thumb-gray-200 scrollbar-track-gray-100"
+      class="w-full min-w-fit md:min-w-[60rem] h-[50rem] overflow-auto scrollbar scrollbar-thumb-gray-200 scrollbar-track-gray-100 scrollbar-thumb-rounded-lg scrollbar-track-rounded-lg"
     >
       <h3 class="my-4 text-xl font-bold">快捷键列表</h3>
-      <div class="grid grid-flow-row auto-rows-auto gap-4">
+      <div class="grid grid-flow-row auto-rows-auto gap-4 p-2">
         <div>
           <h4 class="my-4 text-lg font-bold">工具</h4>
           <div class="border rounded-lg divide-y">
--- pictode/tailwind.config.cjs
+++ pictode/tailwind.config.cjs
@@ -4,5 +4,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require('tailwind-scrollbar')],
+  plugins: [require('tailwind-scrollbar')({ nocompatible: true })],
 };
