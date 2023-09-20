12560bd91966dee782cd1f73a891169682215bdf
Yan Heng <yanheng@siactpower.com>
2023-09-20 13:59:24 +0800

    update: 新增日语/法语语言包

--- pictode/src/locales/index.ts
+++ pictode/src/locales/index.ts
@@ -1,6 +1,8 @@
 import { createI18n } from 'vue-i18n';
 
 import en from './languages/en';
+import fr from './languages/fr';
+import ja from './languages/ja';
 import zhCN from './languages/zh-CN';
 import zhTW from './languages/zh-TW';
 
@@ -8,6 +10,8 @@ export const languages = [
   { label: '简体中文', value: 'zh-CN', message: zhCN },
   { label: '繁体中文', value: 'zh-TW', message: zhTW },
   { label: 'English', value: 'en', message: en },
+  { label: 'Français', value: 'fr', message: fr },
+  { label: '日本語', value: 'ja', message: ja },
 ];
 
 export const i18n = createI18n({
