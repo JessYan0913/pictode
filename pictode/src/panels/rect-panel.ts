c60567dd40d62a1be35cc20e18502dc53052dcc9
JessYan <52144974+JessYan0913@users.noreply.github.com>
2023-10-06 22:50:37 +0800

    update: 更新边框属性面板

--- pictode/src/panels/rect-panel.ts
+++ pictode/src/panels/rect-panel.ts
@@ -56,6 +56,29 @@ export const rectPanelConfig: PanelConfig = {
       ],
     },
     {
+      name: 'dash',
+      label: '边框样式',
+      type: 'RadioGroup',
+      optionType: 'icon',
+      options: [
+        {
+          label: 'line-1',
+          value: [],
+          title: '实线',
+        },
+        {
+          label: 'dotted-line-2',
+          value: [12, 8],
+          title: '虚线',
+        },
+        {
+          label: 'dotted-line-1',
+          value: [6, 4],
+          title: '点虚线',
+        },
+      ],
+    },
+    {
       name: 'opacity',
       label: '透明度',
       type: 'Slider',
@@ -66,6 +89,7 @@ export const rectPanelConfig: PanelConfig = {
     fill: '#ffffff',
     strokeWidth: 2,
     cornerRadius: 20,
+    dash: [],
     opacity: 1,
   },
 };
