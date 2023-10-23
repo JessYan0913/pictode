64e91a5722eb5afd825773fbbc672f373f048a5f
knight.gao <knight.gao@tineco.com>
2023-10-23 10:05:10 +0800

    feat: enlarge/reduce based on the center point of the screen

--- packages/core/src/app.ts
+++ packages/core/src/app.ts
@@ -302,7 +302,10 @@ export class App extends BaseService<EventArgs> {
     return this.stage.scaleX();
   }
 
-  public scaleTo(scale: number, pointer: Point = new Point(0, 0)): App {
+  public scaleTo(
+    scale: number,
+    pointer: Point = new Point(Number(this.stage.width) / 2, Number(this.stage.height) / 2)
+  ): App {
     const oldScale = this.scale();
     this.emit('canvas:zoom:start', { scale: oldScale });
     const newScale = Math.min(Math.max(scale, this.config.scale.min), this.config.scale.max);
