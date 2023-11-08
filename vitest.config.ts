10c45c18c1ae4eb8e6111d0db2ea3fb25379ad9d
JessYan <52144974+JessYan0913@users.noreply.github.com>
2023-11-08 22:04:28 +0800

    update: 更新vitest版本号

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,12 +7,18 @@ const r = (p: string) => resolve(__dirname, p);
 export default defineConfig({
   test: {
     exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**'],
-    include: ['./packages/**/tests/**'],
+    include: ['**/__test__/**'],
     environment: 'jsdom',
   },
   resolve: {
     alias: {
+      '@pictode/core': r('./packages/core/src'),
+      '@pictode/tools': r('./packages/tools/src'),
       '@pictode/utils': r('./packages/utils/src'),
+      '@pictode/vue-aide': r('./packages/vue-aide/src'),
+      '@pictode/plugin-alignment': r('./packages/plugin-alignment/src'),
+      '@pictode/plugin-history': r('./packages/plugin-history/src'),
+      '@pictode/plugin-selector': r('./packages/plugin-selector/src'),
     },
   },
 });
