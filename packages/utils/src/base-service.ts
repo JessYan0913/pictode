6aa4b6d38267987ca5e7ba46c75fb27251236e9d
yanheng <yanheng@siactpower.com>
2024-03-14 13:23:09 +0800

    refactor: 重构函数定义

--- packages/utils/src/base-service.ts
+++ packages/utils/src/base-service.ts
@@ -2,9 +2,7 @@ import { EventArgs, EventBus } from './event';
 
 type Method = (...args: any[]) => any;
 
-export interface Middleware {
-  (args: any[], next: Method): any;
-}
+export type Middleware = (args: any[], next: Method) => any;
 
 export class BaseService<T extends EventArgs = EventArgs> extends EventBus<T> {
   constructor() {
