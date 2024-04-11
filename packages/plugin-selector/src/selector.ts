14a9b3a0fa75d2c702d33ff2fefae1654471c9d2
yanheng <yanheng@siactpower.com>
2024-04-11 13:48:50 +0800

    refactor: 代码重构

--- packages/plugin-selector/src/selector.ts
+++ packages/plugin-selector/src/selector.ts
@@ -1,6 +1,7 @@
 import { App, EventArgs, Konva, KonvaNode, util } from '@pictode/core';
 
 import { HightLightConfig, Options, RubberConfig, TransformerConfig } from './types';
+import { getNodeRect } from './utils';
 
 interface HightLightRect {
   rect: Konva.Rect;
@@ -245,7 +246,7 @@ export class Selector {
   }
 
   private getAbsoluteNodeRect(node: KonvaNode, rect: Konva.Rect, padding: number = 0): void {
-    const { x, y, width, height } = this.getNodeRect(node, padding);
+    const { x, y, width, height } = getNodeRect(node, padding);
     rect.position({
       x: (x - this.app.stage.x()) / this.app.stage.scaleX(),
       y: (y - this.app.stage.y()) / this.app.stage.scaleY(),
@@ -253,70 +254,6 @@ export class Selector {
     rect.width(width / this.app.stage.scaleX());
     rect.height(height / this.app.stage.scaleY());
     rect.rotation(node.rotation());
-  }
-
-  private getNodeRect(
-    node: KonvaNode,
-    padding: number = 0
-  ): {
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-  } {
-    const getAngle = (angle: number): number => {
-      return Konva.angleDeg ? (angle * Math.PI) / 180 : angle;
-    };
-    const totalPoints: Array<util.Point> = [];
-    let nodes: KonvaNode[] = [];
-    if (node instanceof Konva.Group) {
-      nodes = node.getChildren();
-    } else {
-      nodes = [node];
-    }
-    nodes.forEach((node) => {
-      const box = node.getClientRect({
-        skipTransform: true,
-        skipShadow: true,
-        skipStroke: this.transformer.ignoreStroke(),
-      });
-      let points = [
-        { x: box.x, y: box.y },
-        { x: box.x + box.width, y: box.y },
-        { x: box.x + box.width, y: box.y + box.height },
-        { x: box.x, y: box.y + box.height },
-      ];
-      let trans = node.getAbsoluteTransform();
-      points.forEach(function (point) {
-        let transformed = trans.point(point);
-        totalPoints.push(new util.Point(transformed.x, transformed.y));
-      });
-    });
-    const tr = new Konva.Transform();
-    tr.rotate(-getAngle(node.rotation()));
-    let minX: number | undefined;
-    let minY: number | undefined;
-    let maxX: number | undefined;
-    let maxY: number | undefined;
-    totalPoints.forEach(function (point) {
-      let transformed = tr.point(point);
-      if (minX === undefined || minY === undefined || maxX === undefined || maxY === undefined) {
-        minX = maxX = transformed.x;
-        minY = maxY = transformed.y;
-      }
-      minX = Math.min(minX, transformed.x);
-      minY = Math.min(minY, transformed.y);
-      maxX = Math.max(maxX, transformed.x);
-      maxY = Math.max(maxY, transformed.y);
-    });
-    tr.invert();
-    const p = tr.point({ x: (minX ?? 0) - padding, y: (minY ?? 0) - padding });
-    return {
-      x: p.x,
-      y: p.y,
-      width: (maxX ?? 0) - (minX ?? 0) + padding * 2,
-      height: (maxY ?? 0) - (minY ?? 0) + padding * 2,
-    };
   }
 
   private onTransformStart = (): void => {
