e8a9d40523a0cd8a805d37ce33fe524a8ee2774f
yanheng <yanheng@siactpower.com>
2024-09-04 16:22:11 +0800

    style: 更新commitlint配置文件

--- commitlint.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [2, 'always', ['feat', 'update', 'fix', 'refactor', 'style', 'docs', 'chore', 'build', 'test']],
-  },
-};
